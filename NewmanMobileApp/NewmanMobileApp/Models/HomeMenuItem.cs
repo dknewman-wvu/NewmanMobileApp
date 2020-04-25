2b8ee4fc8f4373fae4afba8679016b90fa9e6a28
David Newman <david.newman@wvumedicine.org>
2020-04-25 15:44:47 -0400

    Add project files.

---
+++ NewmanMobileApp/NewmanMobileApp/Models/HomeMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewmanMobileApp.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About, 
+        Quiz
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ NewmanMobileApp/NewmanMobileApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NewmanMobileApp.Models;
+
+namespace NewmanMobileApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
